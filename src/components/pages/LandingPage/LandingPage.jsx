1b61246449a64bb39f6145665185a384a9829b26
Joel <joel2006sm@gmail.com>
2026-04-20 14:53:48 +0000

    landing

--- src/components/pages/LandingPage/LandingPage.jsx
+++ src/components/pages/LandingPage/LandingPage.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
 
   return (
     <div>
-      <nav>
+      <nav className="navbar">
         <a href="#" className="logo-h">
           BarrioRed
         </a>
